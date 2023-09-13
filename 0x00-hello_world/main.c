bae3cc5ea79e0f6a73df7ef2cdf5ee6d9e08f196
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-13 15:32:55 -0700

    Information: How to get a preprocessed file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: main is the standard entry point of C programs
+ * Return: The output of parameters
+ */
 int main(void)
 {
-  /*Display Hello World*/
-  printf("Hello World \n");
-  return 0;
+/*Display Hello World*/
+printf("Hello World \n");
+return (0);
 }
