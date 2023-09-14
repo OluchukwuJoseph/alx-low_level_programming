91c8d64091b3d97665feb30caf05455df192e749
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-13 22:50:44 -0700

    Information : How to use Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - This is the standard entry point (Beginning) of all C programs
+ * Return: Displays the output of Arguments passed
+ */
+int main(void)
+{
+/*sets s as a variable with string data type*/
+char s[] = "Programming is like building a multilingual puzzle";
+/*Prints s*/
+puts(s);
+return (0);
+}
