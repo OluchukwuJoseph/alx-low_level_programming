9878eb711d2a6bb15613595209d965caff29ce2c
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-14 03:27:36 -0700

    Information: How to get size of variables in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int i;
 long long int li;
 float f;
 /*Print size of bytes for specified data types*/
-printf("Size of a char: %li byte(s)\n", sizeof(c));
-printf("Size of an int: %li byte(s)\n", sizeof(i));
-printf("Size of a long long int: %li byte(s)\n", sizeof(li));
-printf("Size of a float: %li byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long long int: %d byte(s)\n", sizeof(li));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 
 }
