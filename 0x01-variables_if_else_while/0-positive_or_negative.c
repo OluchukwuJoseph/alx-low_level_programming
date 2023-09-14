d8303edc84155da786b16444bbad7b0cb8c1c055
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-14 11:32:25 -0700

    Information: Checking for positive and negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /*Checks if n is a positive or negative number*/
 if (n > 0)
-	printf("%d is positive \n", n);
+	printf("%d is positive\n", n);
 else if (n < 0)
-	printf("%d is negative \n", n);
-else
-	printf("%d is zero \n", n);
+	printf("%d is negative\n", n);
+else if (n == 0)
+	printf("%d is zero\n", n);
 return (0);
 }
