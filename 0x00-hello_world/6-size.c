378775717a865b609539b1976f62166a0ad83caf
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-14 03:24:25 -0700

    Information: How to get size of variables in bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the standard Entry point of all C program.
+ * It is the function that gets read first.
+ * Return: The output of arguments in this function
+ */
+int main(void)
+{
+/*Declears c, i, li, f as variables of specified data types*/
+char c;
+int i;
+long long int li;
+float f;
+/*Print size of bytes for specified data types*/
+printf("Size of a char: %li byte(s)\n", sizeof(c));
+printf("Size of an int: %li byte(s)\n", sizeof(i));
+printf("Size of a long long int: %li byte(s)\n", sizeof(li));
+printf("Size of a float: %li byte(s)\n", sizeof(f));
+return (0);
+
+}
