d34636bcb7f49b4e74144636968d65201f8c6374
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-14 02:17:15 -0700

    Information: How to use -Wall and printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the standard Entry point of all C program.
+ * It is the function that gets read first.
+ * Return: The output of arguments in this function
+ */
+int main(void)
+{
+/*Sets s as a variable with string data type*/
+char s[] = "with proper grammar, but the outcome is a piece of art,";
+/*Print value of s*/
+printf("%s \n", s);
+return (0);
+}
