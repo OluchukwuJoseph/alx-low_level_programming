5e3568a233bc7ba869548653a9b4dd473086c689
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-15 14:25:32 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the standard Entry point of all C programs.
+ * The program reads this function first before reading other functions.
+ * Return: return 0 if error is encounted.
+*/
+int main(void)
+{
+/*Prints 0-9*/
+printf("0123456789\n");
+
+return (0);
+}
