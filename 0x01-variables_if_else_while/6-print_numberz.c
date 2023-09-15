bd08397af3882a98abd36f1ac0529c9159023d77
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-15 14:39:08 -0700

    Using for loop with digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the standard Entry point of all C programs.
+ * The program reads this function first before reading other functions.
+ * Return: return 0 if error is encounted.
+*/
+int main(void)
+{
+int num;
+num = 0;
+/*prints 0 - 9*/
+for (; num <= 9; num++)
+{
+	putchar(num);
+}
+putchar('\n');
+return (0);
+}
