11dd78ef9b796b963089171c8ebef5199c6376d1
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-15 22:58:15 -0700

    How to format text when printing with for loop

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the standard Entry point of all C programs.
+ * The program reads this function first before reading other functions.
+ * Return: return 0 if error is encounted.
+*/
+int main(void)
+{
+/*sets n and ln as variables and stores values in them*/
+
+int n, ln;
+n = 0;
+ln = 9;
+
+/*print 0 - 9 in ascending order with comma seperating them*/
+
+for (; n <= ln; n++)
+	if (n < 9)
+	{
+		putchar('0' + n);
+                putchar(',');
+                putchar(' ');
+	}
+	else
+	{
+		putchar('0' + n);
+	}
+
+return (0);
+}
