4b4b8d0174412057efd7957da040f65712e12c20
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-16 18:29:13 -0700

    Add function to print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _putchar - write content of character c
+ * @c: The character to print
+ *
+ * Return: On sucess return 1.
+ * On error, -1 is returned, and error is set approprately.
+ */
+int _putchar(char c);
+int print_alphabet(void);
+/**
+ * main - Entry point
+ *
+ * Description: This is the first Function that gets executed.
+ * All functions and statements that will be printed is be included in the main
+ * Return: 0 on sucess.
+ */
+int main(void)
+{
+	int print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints a - z
+ *
+ * Return: return 1 on sucess
+ */
+int print_alphabet(void)
+{
+	char fc, lc;
+
+	fc = 'a';
+	lc = 'z';
+	for (; fc <= lc; fc++)
+	{
+		_putchar(fc);
+	}
+	_putchar('\n');
+
+	return (1);
+}
