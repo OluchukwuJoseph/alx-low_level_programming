473475d85171001baf6be2bc002806af4ff70a1f
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-18 11:21:25 -0700

    Swaps first value passed to second value passed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value passed in *a to *b
+ * @a: stores value and swaps value passed to @b
+ * @b: stores value and swaps value passed to @a
+ *
+ * Return: return 0 on sucess.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
