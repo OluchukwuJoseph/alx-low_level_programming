932b145d68a74370456270f15873f87f536ae446
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-19 12:42:41 -0700

    Fixed Bug in print_array Function

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,13 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n <= 0)
+	{
 		printf("\n");
+		return;
+	}
+	printf("%d", a[0]);
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i < n; i++)
 		printf(", %d", a[i]);
 
 	printf("\n");
