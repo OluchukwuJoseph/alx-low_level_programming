4e75dd3b51becc88c4273f80e6e937508a5bb468
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-19 06:04:35 -0700

    Function that reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,45 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: stores the contents of string
- *
- * Return: void
+ * rev_string - reverse string by swapping indexes
+ * @s: array that stores string
+ * Return: Void
  */
 void rev_string(char *s)
 {
 	int i;
 	int lenght = 0;
+	char tmp;
 
 	for (i = 0; s[i] != '\0'; i++)
-		lenght++;
-
-	for (i = 0; i < lenght / 2; i++)
 	{
-		char j;
+		lenght++;
+	}
 
-		j = s[i];
-		s[i] = s[count - 1 - i];
-		s[count - 1 - i] = j;
+	/*If lenght is an even number*/
+	if (lenght - 1 % 2 == 0)
+	{
+		for (i = 0, lenght = lenght - 1; i <= lenght; i++, lenght--)
+		{
+			/* store str[i] in tmp tempoarily*/
+			tmp = str[i];
+			/* swaps str[i] to str[lenght]*/
+			str[i] = str[lenght];
+			/* swaps str[lenght] to str[i]*/
+			str[lenght] = tmp;
+		}
+	}
+	/* If lenght is an odd number*/
+	else
+	{
+		for (i = 0, lenght = lenght - 1; i < lenght; i++, lenght--)
+		{
+			/* store str[i] in tmp tempoarily*/
+			tmp = str[i];
+			/* swaps str[i] to str[lenght]*/
+			str[i] = str[lenght];
+			/* swaps str[lenght] to str[i]*/
+			str[lenght] = tmp;
+		}
 	}
 }
