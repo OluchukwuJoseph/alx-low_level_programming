028d4e7b20ed8d44f4b374e3d647899c817c6baa
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-22 06:27:42 -0700

    strcmp clone

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string and prints their ASCII differences
+ * @s1: First string
+ * @s2: Second string
+ * Return: return n.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int n = 0;
+
+	/*Loops through both strings*/
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		/*Checks if string 1 is equal to string 2*/
+		if (s1[i] != s2[i])
+		{
+			n = (int)(unsigned char) *s1 - (int)(unsigned char) *s2;
+			return (n);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+}
