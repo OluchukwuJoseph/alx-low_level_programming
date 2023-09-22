75d5173215f0c0092460aef627fec69ddbe0a63b
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-22 14:51:54 -0700

    Caps First alphabet of eash word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Function changes the First letter of every word to uppercase
+ * @n : string
+ * Return: n
+ */
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] == ' ' ||
+				n[i] == '\t' ||
+				n[i] == '\n' ||
+				n[i] == ',' ||
+				n[i] == ';' ||
+				n[i] == '.' ||
+				n[i] == '?' ||
+				n[i] == '"' ||
+				n[i] == '(' ||
+				n[i] == ')' ||
+				n[i] == '{' ||
+				n[i] == '}')
+		{
+			if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
+				n[i + 1] = n[i + 1] - 32;
+		}
+	}
+	return (n);
+}
