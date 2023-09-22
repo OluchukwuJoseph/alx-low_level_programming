85e27af8089f2558dc66c2fc6c83628b75713974
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-09-22 12:59:27 -0700

    strcmp clone

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int n = 0;
 
 	/*Loops through both strings*/
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		/*Checks if string 1 is equal to string 2*/
 		if (s1[i] != s2[i])
 		{
-			n = (int)(unsigned char) *s1 - (int)(unsigned char) *s2;
-			return (n);
+			return (s1[i] - s2[i]);
 		}
 
 	}
