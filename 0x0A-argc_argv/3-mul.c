5669d2b1b792174986abd89dd233c4fdbfb77791
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-04 05:57:29 -0700

    Multiply strings and print result

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiply first 2 strings
+ * @argc: Argument counter
+ * @argv: Argument Vector
+ * Return: 0 on Sucess and 1 on Error
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3)
+	{
+		fprintf(stderr, "Error");
+		return (1);
+	}
+	if (argc > 2)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
