f9bca203821550ed4f0ba75ab3be19a7a0a8e7d2
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-04 06:50:02 -0700

    Handled Malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+        	/* malloc failed, handle the error */
+        	return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
