89bb829c51316b4b583bd7338ac40199d4dcf3a3
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-04 06:53:29 -0700

    Fixed Betty Coding Style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ char *create_array(unsigned int size, char c)
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
-        	/* malloc failed, handle the error */
-        	return (NULL);
+		/* malloc failed, handle the error */
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 		array[i] = c;
