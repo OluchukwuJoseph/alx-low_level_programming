c35053e49e89d1b7d93e9852481ee7993aab2bb3
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-04 10:38:10 -0700

    strdup clone

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate string passed and returns pointer to string location
+ * @str: string
+ * Return: pointer to string location, NULL on error
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i, lenght = 0;
+	/*Counts string*/
+	for (i = 0; str[i] != '\0'; i++)
+		lenght++;
+	/*Checks if string is empty*/
+	if (lenght == 0)
+		return (NULL);
+	/*Allocate memory for string*/
+	array = malloc(lenght * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < lenght; i++)
+		array[i] = str[i];
+
+	return (array);
+}
