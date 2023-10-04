f8e65ff39f6630e48cb79237308da32d28938a4c
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-04 14:23:39 -0700

    Bug Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,32 +12,24 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int i, lenght1 = 0, lenght2 = 0;
 
-	/*counts first string*/
-	for (i = 0; s1[i] != '\0'; i++)
-		lenght1++;
-	/*counts second string*/
-	for (i = 0; s2[i] != '\0'; i++)
-		lenght2++;
-	/*checks if first string and second string is empty*/
-	if (lenght1 == 0 && lenght2 == 0)
-		return (NULL);
 	/*checks if first string and second string is NULL*/
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	/*checks if first string is empty and second string is NULL*/
-	if (lenght1 == 0 && s2 == NULL)
-		return (NULL);
-	/*checks if first string is NULL and second string is empty*/
-	if (s1 == NULL && lenght2 == 0)
-		return (NULL);
-	/*checks if first string is NULL and second string is not empty*/
-	if (s1 == NULL && lenght2 > 0)
-		lenght1 = 0;
-	/*checks if first string is not empty and second string is NULL*/
-	if (lenght1 > 0 && s2 == NULL)
-		lenght2 = 0;
 
-	lenght2++;
+	if (s1 != NULL)
+	{
+		/*counts first string*/
+		for (i = 0; s1[i] != '\0'; i++)
+			lenght1++;
+	}
+
+	if (s2 != NULL)
+	{
+		/*counts second string*/
+		for (i = 0; s2[i] != '\0'; i++)
+			lenght2++;
+	}
+
 	array = malloc((lenght1 + lenght2) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
@@ -48,5 +40,6 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < lenght2; i++)
 		array[lenght1 + i] = s2[i];
 
+	array[lenght1 + lenght2] = '\0';
 	return (array);
 }
