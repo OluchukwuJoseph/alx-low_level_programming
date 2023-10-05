3d70b04145f4da2559567c13e7bdbc0dcfda9a09
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-05 10:58:04 -0700

    function that concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenates strings passed in command line
+ * @ac: argument counter
+ * @av: argument vector (array)
+ * Return: concatenated string
+*/
+char *argstostr(int ac, char **av)
+{
+	char *array;
+	int c, s, lenght = 0, i = 0;
+
+	/*checks if no argument or filename was passed*/
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/*count how many characters is in each string*/
+	for (c = 0; c < ac; c++)
+	{
+		for (s = 0; av[c][s] != '\0'; s++)
+			lenght++;
+	}
+
+	/*allocate space for characters*/
+	array = malloc((lenght + ac) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
+	/*concatenate characters*/
+	for (c = 0; c < ac; c++)
+	{
+		for (s = 0; av[c][s] != '\0'; s++, i++)
+			array[i] = av[c][s];
+		array[i] = '\n';
+		i++;
+	}
+	return (array);
+}
