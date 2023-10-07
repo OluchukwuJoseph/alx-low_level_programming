604a933284a19124526abb8a810c94c9bea5afd0
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-07 12:53:29 -0700

    Negative Number Bug Fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,16 +9,23 @@
 */
 int main(int ac, char *av[])
 {
-	int i, j, n, number = 0, result = 0;
+	int i, n, result = 0;
 	/*Checks if any argument was passed*/
 	if (ac == 1)
 		return (0);
 	/*Checks for numbers in the array and converts them to integer*/
 	for (i = 1; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		int j = 0, is_negative = 0, number = 0;
+
+		if (av[i][j] == '-')
 		{
-			if (av[i][j] <= '9')
+			is_negative = 1;
+			j++;
+		}
+		for (; av[i][j] != '\0'; j++)
+		{
+			if (av[i][j] >= '0' && av[i][j] <= '9')
 			{
 				n = (av[i][j] - '0');
 				number = number * 10 + n;
@@ -29,6 +36,8 @@ int main(int ac, char *av[])
 				return (1);
 			}
 		}
+		if (is_negative == 1)
+			number = -number;
 		/*sum intergers*/
 		result = result + number;
 		number = 0;
