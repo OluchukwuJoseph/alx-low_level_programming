72133283c56c41576189def1efc59aff73cef6c0
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-09 12:39:32 -0700

    Multiplication Function

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,7 +15,7 @@ int _isdigit(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] < 0 || s[i] > 9)
+		if (s[i] < '0' || s[i] > '9')
 		return (0);
 	}
 	return (1);
@@ -56,15 +56,15 @@ int main(int ac, char *av[])
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+	s1 = av[1], s2 = av[2];
+	if (ac != 3 || !_isdigit(s1) || !_isdigit(s2))
 		error();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(len * sizeof(int));
 	if (result == NULL)
-		return (NULL);
+		return (1);
 	for (i = 0; i <= (len1 + len2); i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
