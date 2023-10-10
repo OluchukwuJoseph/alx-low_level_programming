c15d9fe7309d19cb11c81cde6ada018433be7cdf
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-10 14:45:14 -0700

    Function that initializes struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - stores parameters passed in struct
+ * @d: struct name
+ * @name: name (struct element)
+ * @age: age (struct element)
+ * @owner: owner (struct element)
+ * Return: nothing
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
