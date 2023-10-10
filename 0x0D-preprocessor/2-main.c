abe48fbb8ba5398b664509c77a64c1de8390fa64
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-10 10:21:46 -0700

    Function that prints source code file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints source code file name
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char *array;
+
+	array = __FILE__;
+	for (i = 0; array[i] != '\0'; i++)
+		_putchar(array[i]);
+	_putchar('\n');
+}
