ad70657cca6e9d5a19ec4107620928465a592fe3
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-11 13:00:30 -0700

    Function that prints struct element values

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the element value of struct passed
+ * d: struct
+ * Return: nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	printf("Name: %s\n",d->name);
+	if (d->age <= 0)
+	{
+		printf("Age: (Nil)\n");
+	}
+	printf("Age: %f\n",d->age);
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Owner: %s\n",d->owner);
+}
+
