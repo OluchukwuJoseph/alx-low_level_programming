6d22a3454e3594e658b8dbbaaa5653de4c71a057
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-11 15:41:10 -0700

    Bug Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,19 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - return the lenght of a string
- * @s: string
- * Return: lenght
- */
-int _strlen(char *s)
-{
-	int i, lenght = 1;
-
-	for (i = 0; s[i] != '\0'; i++)
-		lenght++;
-	return (lenght);
-}
-/**
  * new_dog - stores parameters passed in struct
  * @name: name (struct element)
  * @age: age (struct element)
@@ -26,29 +13,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	unsigned int n1, o1, i;
 
-	n = _strlen(name);
-	o = _strlen(owner);
-
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	/*allocates memory to new_dog*/
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		free(new_dog);
 		return (NULL);
 	}
-
+	for (n1 = 0; name[n1]; n1++)
+		n1++;
 	new_dog->name = malloc(n1 * sizeof(char));
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n1; i++)
 		new_dog->name[i] = name[i];
 	new_dog->age = age;
+	for (o1 = 0; owner[o1]; o1++)
+		o1++;
 	new_dog->owner = malloc(o1 * sizeof(char));
 	if (new_dog->owner == NULL)
 	{
@@ -56,7 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i < o; i++)
+	for (i = 0; i < o1; i++)
 		new_dog->owner[i] = owner[i];
 
 	return (new_dog);
