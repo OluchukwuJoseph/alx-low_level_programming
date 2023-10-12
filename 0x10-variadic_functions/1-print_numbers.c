bb124c53e3fe24ab595aa05b04c9c70a9ef0267c
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-12 13:08:45 -0700

    Modified file 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,31 +11,22 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	int *num_array;
 	unsigned int i;
 
 	va_start(args, n);
 	if (n == 0)
-		return;
-	if (separator == NULL)
-		separator = "";
-	num_array = malloc(n * sizeof(int));
-	if (num_array == NULL)
 	{
-		printf("Insufficient space\n");
-		free(num_array);
 		return;
 	}
+	if (separator == NULL)
+		separator = ""; 
 
-	for (i = 0; i < n; i++)
-		num_array[i] = va_arg(args, int);
-	printf("%d", num_array[0]);
+	printf("%d", va_arg(args, int));
 	for (i = 1; i < n; i++)
 	{
 		printf("%s", separator);
-		printf("%d", num_array[i]);
+		printf("%d", va_arg(args, int));
 	}
 	printf("\n");
-	free(num_array);
 	va_end(args);
 }
