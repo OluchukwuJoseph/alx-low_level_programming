638b90bc98643edb31c05f8a36fc677e51a3546f
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-12 12:25:00 -0700

    Added: File 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints numbers passed with seperators in between
+ * @separator: characters between numbers
+ * @n: number of arguments passed
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int *num_array;
+	unsigned int i;
+
+	va_start(args, n);
+	if (n <= 0)
+	{
+		printf("NULL");
+		return;
+	}
+	num_array = malloc(n * sizeof(int));
+	if (num_array == NULL)
+	{
+		printf("Insufficient space");
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+		num_array[i] = va_arg(args, int);
+	printf("%d", num_array[0]);
+	for (i = 1; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%s", separator);
+		printf("%d", num_array[i]);
+	}
+	printf("\n");
+	free(num_array);
+}
