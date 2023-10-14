df5b8403ed73b0fde978f0decf4168e7cdbe2675
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-13 19:58:35 -0700

    Added: file 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+
+/**
+ * int_index - executes array in function passed
+ * @array: Array of integers to be passed in function
+ * @size: size of array
+ * @cmp: function to be executed
+ * Return: i
+ * long description: It executes an array in function passed,
+ * and if the function returns 1,
+ * it returns the array's index.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int value = 0, i = 0, tmp = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		tmp = array[i];
+		value = cmp(tmp);
+		if (value == 1)
+			break;
+		i++;
+	}
+	return (i);
+}
