4a0fba4a05f9568463b98a9a5453ed902ce47a11
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-20 14:57:19 -0700

    Modifie File 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
-size_t print_list(node *head)
+size_t print_list(const list_t *h)
 {
-	node *last;
+	const list_t *current = h;
 	size_t i = 0;
 
-	last = head;
-	while (last != NULL)
+	while (current != NULL)
 	{
-		if (last -> string == NULL)
+		if (current->str == NULL)
 		{
-			last -> string = "(nil)";
-			last -> length = 0;
+			printf("[0] (nil)\n");
 		}
-	printf("[%d] %s\n", last -> length, last -> string);
-	i++;
-	last = last -> ptr;
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		i++;
+		current = current->next;
 	}
 
 	return (i);
