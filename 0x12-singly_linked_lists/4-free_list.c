e689fdbc8cd11ad588b2638661505660c1407117
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-20 20:04:29 -0700

    Added File 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: Beginning of linked list
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *temp;
+
+	current = head;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+}
