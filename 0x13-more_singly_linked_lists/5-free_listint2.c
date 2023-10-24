403e9515cbb4c7a1c5020b2c7667cc2c414dd7ee
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-24 00:53:49 -0700

    Added documentation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - Frees a linked list and sets head to NULL
+ * @head: Beginning of linked list
+ * Return: nothing
+*/
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
