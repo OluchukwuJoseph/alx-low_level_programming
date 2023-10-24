5e3525e9f839731a0d615387adcdf1cf34bc811e
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-24 15:23:13 -0700

    Changed logic in File 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,65 +1,41 @@
 #include "lists.h"
 
 /**
- * add_front - adds node to the beginning of the list
- * @head: Address of first node in the list
- * @n: Value to be stored at new node
- * Return: Address of new node
-*/
-listint_t *add_front(listint_t **head, int n)
-{
-	listint_t *new_node;
-
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	new_node = (listint_t *)malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
-
-/**
- * insert_nodeint_at_index - inserts node at index specified
- * @head: Address of first node in the list
- * @idx: index to add new node
- * @n: Value to be stored at new node
- * Return: Address of new node
-*/
+ * insert_nodeint_at_index - inserts a node at the specified index
+ * @head: Address of a pointer to the first node in the list
+ * @idx: Index to add the new node
+ * @n: Value to be stored at the new node
+ * Return: Address of the new node, or NULL on failure
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp = *head, *tmp, *new_node;
-	unsigned int i = 0, j = 0;
+	listint_t *temp = *head, *new_node;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
-	if (idx == 0)
-	{
-		temp = add_front(head, n);
-		return (temp);
-	}
-	while (temp != NULL)
-	{
-		i++;
-		temp = temp->next;
-	}
-	if (i < idx)
-		return (NULL);
-	temp = *head;
-	while (j != (idx - 1))
-	{
-		temp = temp->next;
-		j++;
-	}
-	tmp = temp->next;
+
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = tmp;
+	if (idx == 0 || *head == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		temp = temp->next;
+	}
+
+	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
 }
