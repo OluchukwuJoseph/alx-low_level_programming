1b6df16fdd28d6fc53a979d66600360558d0a9d3
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-24 00:35:26 -0700

    Modified Conditional statement in File 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,12 @@ void free_listint2(listint_t **head)
 	if (*head == NULL || head == NULL)
 		return;
 
-	while (*head)
+	while ((*head)->next != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+	free(*head);
 	*head = NULL;
 }
