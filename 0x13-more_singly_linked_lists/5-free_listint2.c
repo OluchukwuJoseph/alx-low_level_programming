39fc9978bd9eed42d3fcc70ab1ef6219ad090c4d
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-24 00:51:48 -0700

    Modified Conditional statement

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,15 @@
 #include "lists.h"
 
-/**
- * free_listint2 - Frees a linked list and sets head to NULL
- * @head: Beginning of linked list
- * Return: nothing
-*/
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = NULL;
+	listint_t *temp;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return;
-
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	free(*head);
-	*head = NULL;
 }
