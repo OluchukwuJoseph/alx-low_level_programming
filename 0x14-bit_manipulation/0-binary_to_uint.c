70190c33d67bc559c7a097ce0a8be947c583d346
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-26 14:30:02 -0700

    Added File 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the characters in a string
+ * @string: string to be counted
+ * Return: Lenght to string
+*/
+unsigned int _strlen(const char *string)
+{
+	unsigned int length = 0;
+
+	while (string[length] != '\0')
+	{
+		if (string[length] == '0' || string[length] == '1')
+			length++;
+		else
+			return (0);
+	}
+	return (length);
+}
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int length = _strlen(b), decimal = 0;
+	unsigned int i = 0;
+
+	for (; i < length; i++)
+		decimal = (decimal << 1) | (b[i] - '0');
+
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This directory will help you fully understand what bitwise operators are and what they are capable of.
