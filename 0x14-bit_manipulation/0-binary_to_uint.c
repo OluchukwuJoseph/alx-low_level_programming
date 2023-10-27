1a6eec34d29d0c720e7ce32aaecf78ddb5943a0e
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-26 22:03:30 -0700

    Added File 100

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -29,7 +29,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int length = _strlen(b), decimal = 0;
 	unsigned int i = 0;
 
-	if (!b)
+	if (!b || b == NULL)
 		return (0);
 
 	for (; i < length; i++)
