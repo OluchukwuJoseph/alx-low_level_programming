a41347375970add8a94ed6dabeb9203d516444bf
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-30 14:17:07 -0700

    Added Function 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the characters in a string
+ * @string: string to be counted
+ * Return: length of string
+ */
+size_t _strlen(const char *string)
+{
+	size_t length = 0;
+
+	while (string[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * create_file - Creates file if file does not exist and adds content to file
+ * @filename: name of file to be created
+ * @text_content: Content to be added
+ * Return: 1 on sucess and -1 otherwise
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t lenght = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (lenght > 0)
+	{
+		text_content[lenght] = '\0';
+		lenght++;
+	}
+	write(fd, text_content, lenght);
+	close(fd);
+	return (1);
+}
