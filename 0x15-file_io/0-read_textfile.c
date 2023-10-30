92024007b9f3f53d7e1214a1134113a8fb9a3fcd
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-30 13:48:08 -0700

    Modified File 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,39 +8,42 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file_d;
-	char character, *buffer;
-	size_t i = 0, lenght = 0, lenght1 = 0;
 	int fd;
+	size_t length = 0, length1;
+	char *buffer;
 
-	file_d = fopen(filename, "r");
-	while ((character = fgetc(file_d)) != EOF)
-	{
-		if (lenght < letters)
-			lenght++;
-	}
-	fclose(file_d);
-	buffer = malloc(sizeof(char) * lenght);
-	if (buffer == NULL)
+	if (letters <= 0 || filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
+		close(fd);
+		return (0);
+	}
+	length = lseek(fd, 0, SEEK_END);
+	if (!(length > 0))
+	{
+		close(fd);
+		return (0);
+	}
+	if (length > letters)
+		length = letters;
+	buffer = (char *)malloc(sizeof(char) * length);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lseek(fd, 0, SEEK_SET);
+	length1 = read(fd, buffer, length);
+	if (!(length1 > 0))
+	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
-	lenght1 = read(fd, buffer, lenght);
-	if (lenght1 < 1)
-	{
-		free(buffer);
-		return (0);
-	}
-	while (i < lenght)
-	{
-		putchar(buffer[i]);
-		i++;
-	}
+	write(STDOUT_FILENO, buffer, length);
 	close(fd);
 	free(buffer);
-	return (i);
+	return (length);
 }
