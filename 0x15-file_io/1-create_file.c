facfe15a198d2a684d8aa3706ed4e7c77bf90011
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-30 14:21:21 -0700

    Modified File 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,11 +31,10 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	if (lenght > 0)
-	{
-		text_content[lenght] = '\0';
-		lenght++;
-	}
+	if (lenght == 0)
+		return (1);
+	text_content[lenght] = '\0';
+	lenght++;
 	write(fd, text_content, lenght);
 	close(fd);
 	return (1);
