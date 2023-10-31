d9eaee2bf3b3f2148c8cc197d534f94a8caa30cf
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-30 21:34:36 -0700

    Modified File 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,33 +13,33 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2 ,"Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	if (av[1] == NULL)
 	{
-		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	if (av[2] == NULL)
 	{
-		dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
 		exit(99);
 	}
 
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
 		exit(99);
 	}
 
@@ -48,24 +48,24 @@ int main(int ac, char **av)
 		bytes_written = write(fd2, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
 			exit(99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (close(fd1) == -1)
 	{
-		dprintf(2 , "Error: Can't close fd %d\n", fd1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
 	if (close(fd2) == -1)
 	{
-		dprintf(2 , "Error: Can't close fd %d\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
