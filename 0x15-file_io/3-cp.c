abfdedc92bd5d04ab1aa3b25f6f6f5c514b7b3c3
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-10-30 21:25:22 -0700

    Added file 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * main - Copies the contents of file_from to file_to
+ * @ac: Argument counter
+ * @av: Arguments
+ * Return: 0 on sucess
+*/
+int main(int ac, char **av)
+{
+	int fd1, fd2, bytes_written, bytes_read;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2 ,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	if (av[1] == NULL)
+	{
+		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (av[2] == NULL)
+	{
+		dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+		exit(99);
+	}
+
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(fd2, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(2 , "Error: Can't write from file %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(2 , "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(2 , "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(2 , "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
