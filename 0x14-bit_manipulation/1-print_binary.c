9cb343ef5e69e05ed8314c8e4b6f3f9a87d3f53d
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-11-23 15:10:56 -0800

    Added add_pow() to 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
 /**
+ * add_pow - This function calculates @base ^ @power
+ * @base: base of exponent
+ * @power: power of exponent
+ * Return: Value of power * base
+ */
+unsigned long int add_pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+	{
+		num = num * base;
+	}
+	return (num);
+}
+/**
  * print_binary - This function prints the binary representation of a number
  * @n: The Number
  * Return: Nothing
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int bitposition = sizeof(unsigned long int) * 8 - 1;
-	unsigned long int mask = 1 << bitposition;
-	int started = 0;
+	unsigned long int mask, check;
+	char started = 0;
 
-	/*checks if zero or a negative number was passed*/
-	if (n <= 0)
-	{
-		putchar('0');
-		return;
-	}
+	mask = add_pow(2, sizeof(unsigned long int) * 8 - 1);
 	/*converts number to binary*/
 	while (mask != 0)
 	{
-		if (n & mask)
+		check = n & mask;
+		if (check == mask)
 		{
 			_putchar('1');
 			started = 1;
 		}
-		else if (started)
+		else if (started == 1 || mask == 1)
 		{
 			_putchar('0');
 		}
