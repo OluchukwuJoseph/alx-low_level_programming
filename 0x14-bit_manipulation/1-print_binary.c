68858b72cf5d86d9a078923fcb1bf137b6c1b7c1
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-11-23 14:33:46 -0800

    Added Files for Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - This function prints the binary representation of a number
+ * @n: The Number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bitposition = sizeof(unsigned long int) * 8 - 1;
+	unsigned long int mask = 1 << bitposition;
+	int started = 0;
+
+	/*checks if zero or a negative number was passed*/
+	if (n <= 0)
+	{
+		putchar('0');
+		return;
+	}
+	/*converts number to binary*/
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			putchar('1');
+			started = 1;
+		}
+		else if (started)
+		{
+			putchar('0');
+		}
+		mask = mask >> 1;
+	}
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -5,3 +5,6 @@ These functions includes:
 unsigned int binary_to_uint(const char *b);
 	USAGE:	This function converts binary numbers to decimal.
 	file:	0-binary_to_uint.c
+void print_binary(unsigned long int n);
+	USAGE: This function prints the binary representation of a number
+	file: 1-print_binary.c
