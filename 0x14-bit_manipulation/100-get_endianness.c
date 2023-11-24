ce9d78f81777517ab3fc868233371432b879522f
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-11-23 20:59:35 -0800

    Added files for Task 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "imain.h"
+
+/**
+ * get_endianness - checkswhat order bytes are stored in computer memory
+ * Return - 1 if it is little Endian and 0 otherwise
+ */
+int get_endianness(void)
+{
+    int num = 1;
+    /* Use a pointer to interpret the integer as an array of characters*/
+    char *byte = (char *)&num;
+
+    /*If the first byte (lowest address) is 1, it's little-endian*/
+    return (*byte == 1);
+}
