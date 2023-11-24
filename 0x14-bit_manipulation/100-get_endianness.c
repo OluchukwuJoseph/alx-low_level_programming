be588d09ce71459eb0fc85379d465f361b58c2af
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-11-23 21:03:23 -0800

    Modified code to pass betty checks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,14 +2,14 @@
 
 /**
  * get_endianness - checkswhat order bytes are stored in computer memory
- * Return - 1 if it is little Endian and 0 otherwise
+ * Return: 1 if it is little Endian and 0 otherwise
  */
 int get_endianness(void)
 {
-    int num = 1;
-    /* Use a pointer to interpret the integer as an array of characters*/
-    char *byte = (char *)&num;
+	int num = 1;
+	/* Use a pointer to interpret the integer as an array of characters*/
+	char *byte = (char *)&num;
 
-    /*If the first byte (lowest address) is 1, it's little-endian*/
-    return (*byte == 1);
+	/*If the first byte (lowest address) is 1, it's little-endian*/
+	return (*byte);
 }
