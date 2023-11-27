989db575a38deba26254817c42ba8eff232bebfd
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-11-27 13:23:31 -0800

    Added Files for Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * create_file - Creates file and adds contents to file
- * @filename: File to be created
- * @textcontent: Content to be added to file
- * Return: 1 on sucess and -1 on failure
+ * append_text_to_file - appends contents to file
+ * @filename: File to append contents to
+ * @textcontent: Content to be appended
+ * Return: 1 on sucess amd -1 on failure
  */
-int create_file(const char *filename, char *textcontent)
+int append_text_to_file(char *filename, char *textcontent)
 {
-	int fd;
 	ssize_t buffer_length = 0, bytes_written = 0;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+
 	if (textcontent == NULL)
 		textcontent = "";
+
 	for (; textcontent[buffer_length]; buffer_length++)
 		;
 	bytes_written = write(fd, textcontent, buffer_length);
