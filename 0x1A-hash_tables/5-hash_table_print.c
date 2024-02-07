9a08c6d38bb5f9cb80a58874eb9decfc3451ad99
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-02-07 19:11:04 +0100

    Fixed bug in Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,9 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int i = 0, printed_pairs = 0;
 	hash_node_t *temp;
 
-	if (ht == NULL)
-		printf("{}\n");
-	else
+	if (ht != NULL)
 	{
 		printf("{");
 		for (; i < ht->size; i++)
