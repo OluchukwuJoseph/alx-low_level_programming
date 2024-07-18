5713aee5bcdcbaa97af8f0828cf75c62483d46a5
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-18 17:24:43 +0100

    Added file for Task 2 and 3

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t i = 0;
 	size_t high = size - 1;
-	size_t mid = low + high / 2;
+	size_t mid = (high + low) / 2;
 
 	if (array == NULL)
 		return (-1);
